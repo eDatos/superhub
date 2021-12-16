ebbc157569b7a2808d4e8337efd1a29f688d39ff
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-12-16 11:02:51 +0000

    Add cli management

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-import sys
+import logzero
+import typer
 
 from superhub import dispatcher, utils
 
 logger = utils.init_logger()
+app = typer.Typer(add_completion=False)
 
-disp = dispatcher.Dispatcher()
-disp.scrap_all(filter=[sys.argv[1]])
+
+@app.command()
+def run(
+    trades: list[str] = typer.Option(
+        [], '--trades', '-t', help='Trades to be scraped. Empty value implies all trades.'
+    ),
+    verbose: bool = typer.Option(
+        False, '--verbose', '-v', show_default=False, help='Loglevel increased to debug.'
+    ),
+):
+    logger.setLevel(logzero.DEBUG if verbose else logzero.INFO)
+    disp = dispatcher.Dispatcher()
+    disp.scrap_all(filter=trades)
+
+
+if __name__ == "__main__":
+    app()
